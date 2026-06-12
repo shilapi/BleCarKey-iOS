0898b27df8399c4fdf81ba98799df130aa58d135
shilapi <60840908+shilapi@users.noreply.github.com>
2026-06-12 16:23:10 +0800

    Add apple watch project; Update login page

---
+++ BaoJunKey Watch App/BaoJunKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaoJunKeyApp.swift
+//  BaoJunKey Watch App
+//
+//  Created by Shilapi Chen on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct BaoJunKey_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
